02e5460a71ac3c6932e2af7ee7682cedc00a10f5
Felipe Freitas <afelipe.freitas@gmail.com>
2021-05-22 16:55:27 -0300

    Modal Component Structures

--- src/App.tsx
+++ src/App.tsx
@@ -24,43 +24,72 @@ function App() {
   const [modalShow, setModalShow] = useState(false);
   const [searchInput, setSearchInput] = useState("");
   const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [paginatedProducts, setPaginatedProducts] = useState([]);
   const [pageCount, setPageCount] = useState(1);
   const [offset, setOffset] = useState(0);
+  const [selectedProduct, setSelectedProduct] = useState<Ferramenta>()
   const [lastVisited, setLastVisited] = useState([]);
 
   const perPage = 12;
 
-  async function fetchData() {
-    try {
-      let response = await axios.get(
-        "https://pluga.co/ferramentas_search.json"
-      );
-      const data = response.data;
-
-      const slice = data.slice(offset, offset + perPage);
-
-      console.log(slice);
-
-      setProducts(slice);
-
-      setPageCount(Math.ceil(data.length / perPage));
-    } catch (err) {
-      console.log(err);
+  useEffect(() => {
+    function searchFilter() {
+      setOffset(0)
+      let result = products.filter((element: Ferramenta) => {
+        return element.name.toLowerCase().includes(searchInput);
+      });
+      setFilteredProducts(result);
     }
-  }
+
+    searchFilter();
+  }, [searchInput, products]);
 
   useEffect(() => {
+    async function fetchData() {
+      try {
+        let response = await axios.get(
+          "https://pluga.co/ferramentas_search.json"
+        );
+        const data = response.data;
+
+        setProducts(data);
+        setFilteredProducts(data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
     fetchData();
-  }, [offset]);
+  }, []);
+
+  useEffect(() => {
+    function paginator() {
+      const slice = filteredProducts.slice(offset, offset + perPage);
+      setPaginatedProducts(slice);
+      setPageCount(Math.ceil(filteredProducts.length / perPage));
+    }
+
+    paginator();
+  }, [offset, filteredProducts]);
 
   interface Event {
     [x: string]: any;
   }
 
   function handlePageClick(e: Event) {
-    console.log(e.target.innerHTML);
-    const selectedPage = e.target.innerHTML;
-    setOffset(selectedPage + 1);
+    const selectedPage = Number(e.target.innerHTML);
+    setPageCount(selectedPage)
+    setOffset((selectedPage - 1) * perPage);
+  }
+
+  function handleProductClick(e: Event) {
+    let selectedProduct = products.filter((element: Ferramenta) => {
+      return element.name === e.target.innerHTML;
+    });
+    setSelectedProduct(selectedProduct[0])
+    setLastVisited(lastVisited => [...lastVisited, ...selectedProduct])
+    setModalShow(true)
   }
 
   return (
@@ -75,22 +104,24 @@ function App() {
           aria-label="Large"
           aria-describedby="inputGroup-sizing-sm"
           placeholder="BUSCAR FERRAMENTA"
-          // onChange={() => setSearchInput(input)}
+          onChange={(event) => setSearchInput(event.target.value)}
         />
       </InputGroup>
 
       <div className="d-flex row justify-content-around mb-5">
-        {products.map((element: Ferramenta) => {
+        {paginatedProducts.map((element: Ferramenta) => {
           return (
-            <Card
-              className="card col-md-2 col-12 m-4 p-3 align-items-center"
-            >
+            <Card className="card col-md-2 col-11 m-4 p-3 align-items-center" style={{backgroundColor: '#D8D8D8'}}>
               <Card.Img
                 variant="top"
                 src={element.icon}
                 style={{ width: "100px" }}
               />
-              <Button variant="primary" onClick={() => setModalShow(true)} className='mt-3'>
+              <Button
+                variant="outline-primary"
+                onClick={handleProductClick}
+                className="mt-3"
+              >
                 {element.name}
               </Button>
             </Card>
@@ -98,7 +129,10 @@ function App() {
         })}
       </div>
 
-      <ModalComponent show={modalShow} onHide={() => setModalShow(false)} />
+      <ModalComponent show={modalShow} onHide={() => setModalShow(false)} 
+      selectedProduct={selectedProduct}
+      lastVisited={lastVisited}
+      />
       <div className="d-flex justify-content-around">
         <PaginationComponent
           handlePageClick={handlePageClick}
--- src/components/ModalComponent/index.tsx
+++ src/components/ModalComponent/index.tsx
@@ -2,12 +2,22 @@ import React from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 
-interface Props{
+type Ferramenta = {
+  app_id: string;
+  name: string;
+  color: string;
+  icon: string;
+  link: string;
+};
+
+interface Props {
   // title: string
-  [x:string]: any;
+  selectedProduct?: Ferramenta;
+  lastVisited: Array<any>;
+  [x: string]: any;
 }
 
-function ModalComponent(props: Props) {
+function ModalComponent({ selectedProduct, lastVisited, ...props }: Props) {
   return (
     <Modal
       {...props}
@@ -15,22 +25,55 @@ function ModalComponent(props: Props) {
       aria-labelledby="contained-modal-title-vcenter"
       centered
     >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
-        </Modal.Title>
-      </Modal.Header>
+      {/* <Modal.Header closeButton /> */}
       <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
-        </p>
+        <div className="mb-5 row flex-wrap justify-content-around">
+          <div className="col-6">
+            <img
+              src={selectedProduct?.icon}
+              style={{ width: "256px" }}
+              alt={selectedProduct?.name}
+            />
+          </div>
+          <div className="column col-6" style={{}}>
+            <h1 className="">{selectedProduct?.name}</h1>
+            <Button variant="primary" size="lg" href={selectedProduct?.link}>
+              Acessar
+            </Button>
+          </div>
+        </div>
+        <hr />
+        <h2>Últimas Ferramentas Visualizadas</h2>
+        <div className="row">
+          {lastVisited.length > 0 ? (
+            lastVisited
+              .slice(Math.max(lastVisited.length - 3, 0))
+              .map((element) => {
+                return (
+                  <div
+                    className="column col-4 justify-content-center"
+                    style={{ backgroundColor: "#D8D8D8" }}
+                  >
+                    <img
+                      src={element.icon}
+                      alt={element.name}
+                      style={{ width: "128px" }}
+                    />
+                    <br />
+
+                    <Button variant="outline-primary" href={element.link}>
+                      {element.name}
+                    </Button>
+                  </div>
+                );
+              })
+          ) : (
+            <div style={{}}>
+              <p className="text-center">Nenhuma Ferramenta Visualizada</p>
+            </div>
+          )}
+        </div>
       </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
     </Modal>
   );
 }
